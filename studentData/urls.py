e7ebdd302c3fcf3e9051ada4eb36ef8d8d1162dd
shobhit <shobhitmishra2002@gmail.com>
2022-08-29 17:48:17 +0530

    added code for the basic ui

---
+++ studentData/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from studentData import views
+
+urlpatterns = [
+    path("",views.index,name="home"),
+    path("addStudent/",views.addStudentFrom, name="add student"),
+    path("student/<int:id>",views.showStudent, name="student"),
+]
